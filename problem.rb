d5c8e16485d3916f02c27b7d52fda1d0207e14a8
yuvraj <yuvraj.167@gmail.com>
2015-10-31 22:17:09 +0530

    second

--- problem.rb
+++ problem.rb
@@ -1,33 +1,36 @@
 require 'date'
-output = []
-week_data = {:Mon => 00,:Tue => 24,:Wed => 48, :Thu => 72,:Fri => 96,:Sat => 120,:Sun => 144}
 
-data = {
-	18..20 => "Mon 1800-2000",
-	120..168 => "Sat-Sun",
-	[8..17, 32..41, 56..65, 80..89, 104..113] => "Mon-Fri 0800-1700",
-	[6..7, 30..31, 54..55, 78..79, 102..103, 126..127, 150..151] => "Mon-Sun 0600-0700",
-	[115..118,139..142] => "Fri-Sat 1900-2200" 
-}
- 
+class Problem
+
+  Week_data = {:Mon => 00,:Tue => 24,:Wed => 48, :Thu => 72,:Fri => 96,:Sat => 120,:Sun => 144}
+  Given_data = {
+  	18..20 => "Mon 1800-2000",
+  	120..168 => "Sat-Sun",
+  	[8..17, 32..41, 56..65, 80..89, 104..113] => "Mon-Fri 0800-1700",
+  	[6..7, 30..31, 54..55, 78..79, 102..103, 126..127, 150..151] => "Mon-Sun 0600-0700",
+  	[115..118,139..142] => "Fri-Sat 1900-2200" 
+  }
+   
   def check_time(input)
-  	day = DateTime.parse(input).strftime("%a") 
-  	time =  DateTime.parse(input).strftime("%H")
-    @week_data[day.to_sym] + time.to_i
-  end
-
-  puts "Enter the time(Format: #{Time.now})"
-  input = gets.chomp
-  formatted_data = check_time(input)
-  
-  data.each do |i,j|
-    if i.is_a?(Range)
-      output << j if i.include?(formatted_data)
-    else
-      i.each {|i| output << j if i.include?(formatted_data)}
+    output = []
+    day = DateTime.parse(input).strftime("%a") 
+    time =  DateTime.parse(input).strftime("%H")
+    formatted_data = Week_data[day.to_sym] + time.to_i
+    Given_data.each do |i,j|
+      if i.is_a?(Range)
+        output << j if i.include?(formatted_data)
+      else
+        i.each {|i| output << j if i.include?(formatted_data)}
+      end
     end
+    puts output.inspect
   end
-  puts output.inspect
 
+end
+
+sample = Problem.new
+sample.check_time('2015-06-29 11:58:47')
+sample.check_time('2015-06-27 06:20:07')
+sample.check_time('2015-05-07 20:10:40')
 
 
---
+++ spec/problem_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../rspec_helper'
+require_relative '../problem.rb'
+
+describe Problem do
+	it '' do
+	end
+
+	it '' do
+	end
+
+end
