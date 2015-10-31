9fd336ec317b8a976207f163ae9a3a0ebdd6a9a5
yuvraj <yuvraj.167@gmail.com>
2015-10-31 21:12:45 +0530

    rspec

--- problem.rb
+++ problem.rb
@@ -1,36 +1,33 @@
-  # Mon 1800-2000
-  # Sat-Sun          
-  # Mon-Fri 0800-1700
-  # Mon-Sun 0600-0700
-  # Fri-Sat 1900-2200
-
 require 'date'
 output = []
-@week_data = {:Mon => 0000,:Tue => 2400,:Wed => 4800, :Thu => 7200,:Fri => 9600,:Sat => 12000,:Sun => 14400}
+week_data = {:Mon => 00,:Tue => 24,:Wed => 48, :Thu => 72,:Fri => 96,:Sat => 120,:Sun => 144}
 
 data = {
-	1800..2000 => "Mon 1800-2000",
-	12000..14400 => "Sat-Sun",
-	800..11300 => "Mon-Fri 0800-1700",
-	600..15100 => "Mon-Sun 0600-0700",
-	11500..14200 => "Fri-Sat 1900-2200" 
+	18..20 => "Mon 1800-2000",
+	120..168 => "Sat-Sun",
+	[8..17, 32..41, 56..65, 80..89, 104..113] => "Mon-Fri 0800-1700",
+	[6..7, 30..31, 54..55, 78..79, 102..103, 126..127, 150..151] => "Mon-Sun 0600-0700",
+	[115..118,139..142] => "Fri-Sat 1900-2200" 
 }
  
   def check_time(input)
   	day = DateTime.parse(input).strftime("%a") 
-  	time =  DateTime.parse(input).strftime("%H00")
-  	#puts day
-  	#puts time
-  	puts @week_data[day.to_sym]
+  	time =  DateTime.parse(input).strftime("%H")
     @week_data[day.to_sym] + time.to_i
   end
 
-  puts "Enter the time"
+  puts "Enter the time(Format: #{Time.now})"
   input = gets.chomp
   formatted_data = check_time(input)
-  #puts formatted_data
-  data.each {|i,j|  output << j if i.include?(formatted_data) }
-  puts output.inspect
   
+  data.each do |i,j|
+    if i.is_a?(Range)
+      output << j if i.include?(formatted_data)
+    else
+      i.each {|i| output << j if i.include?(formatted_data)}
+    end
+  end
+  puts output.inspect
+
 
 
---
+++ problem_spec.rb
@@ -0,0 +1,6 @@
+require_relative 'problem.rb'
+require 'rspec_helper'
+
+describe Problem do
+
+end
